2fa53ddd98ca34c68ce5392fc11bf7b2c9a09692
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-20 16:16:04 +0200

    add mock service worker
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,7 +1,10 @@
 import type { Decorator, Parameters } from "@storybook/react";
+import { initialize, mswDecorator } from "msw-storybook-addon";
 import React from "react";
-import "../src/styles/fonts.css";
 import { GlobalStyle } from "../src/components/GlobalStyle";
+import "../src/styles/fonts.css";
+
+initialize();
 
 export const parameters: Parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -14,6 +17,8 @@ export const parameters: Parameters = {
 };
 
 export const decorators: Decorator[] = [
+  // @ts-expect-error mswDecorator has not updated to the storybook v7 types
+  mswDecorator,
   (Story) => (
     <>
       <GlobalStyle />
