1b5ed5c844e6d84b77e6dc2bb80a5c6eb80a4868
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-20 11:28:04 +0200

    remove test file
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { siteDescription, siteTitle } from "@/constants";
 import Head from "next/head";
-import Logo from "public/assets/logo.svg";
 
 export default function Home() {
   return (
@@ -11,7 +10,6 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Logo />
     </>
   );
 }
