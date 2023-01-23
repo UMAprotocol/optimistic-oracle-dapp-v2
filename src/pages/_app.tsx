0c1db5f644fd13479325b4b6755157362c331170
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-23 11:06:51 +0200

    add and configure rainbowkit
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,56 @@
 import { GlobalStyle } from "@/components";
+import { infuraId } from "@/constants";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import "@rainbow-me/rainbowkit/styles.css";
 import type { AppProps } from "next/app";
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  arbitrum,
+  avalanche,
+  evmos,
+  gnosis,
+  goerli,
+  mainnet,
+  optimism,
+  polygon,
+  polygonMumbai,
+} from "wagmi/chains";
+import { infuraProvider } from "wagmi/providers/infura";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [
+    mainnet,
+    goerli,
+    optimism,
+    gnosis,
+    polygon,
+    evmos,
+    arbitrum,
+    avalanche,
+    polygonMumbai,
+  ],
+  [infuraProvider({ apiKey: infuraId }), publicProvider()]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: "Optimistic Oracle dApp V2",
+  chains,
+});
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </>
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <GlobalStyle />
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
   );
 }
