a2eca2fee719654e27b587bf6c49381ece67d309
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-24 11:30:52 +0200

    add nav style
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Header/Nav.tsx
+++ src/components/Header/Nav.tsx
@@ -1,11 +1,19 @@
-import { navLinks } from "@/constants";
+import { grey400, navLinks, white } from "@/constants";
 import { isExternalLink } from "@/helpers";
 import NextLink from "next/link";
 import ExternalLink from "public/assets/external-link.svg";
 import styled from "styled-components";
+import { useIsClient } from "usehooks-ts";
 
 export function Nav() {
   // todo: add closePanel on routeChangeStart once panel is implemented
+
+  const isClient = useIsClient();
+
+  function isActive(href: string) {
+    if (!isClient) return false;
+    return window.location.href.includes(href);
+  }
   return (
     <Wrapper>
       <NavItems>
@@ -14,6 +22,7 @@ export function Nav() {
             <Link
               href={href}
               target={isExternalLink(href) ? "_blank" : undefined}
+              style={{ "--color": isActive(href) ? white : grey400 }}
             >
               {title} {isExternalLink(href) && <ExternalLinkIcon />}
             </Link>
@@ -24,14 +33,36 @@ export function Nav() {
   );
 }
 
-const Wrapper = styled.nav``;
+const Wrapper = styled.nav`
+  display: flex;
+  align-items: center;
+`;
 
 const NavItems = styled.ul`
   list-style: none;
+  display: flex;
+  align-items: center;
+  gap: 32px;
 `;
 
 const NavItem = styled.li``;
 
-const Link = styled(NextLink)``;
+const Link = styled(NextLink)`
+  display: flex;
+  align-items: center;
+  gap: 4px;
+  text-decoration: none;
+  transition: opacity var(--animation-duration);
+  font: var(--body-md);
+  color: var(--color);
 
-const ExternalLinkIcon = styled(ExternalLink)``;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+
+const ExternalLinkIcon = styled(ExternalLink)`
+  path {
+    stroke: var(--color);
+  }
+`;
