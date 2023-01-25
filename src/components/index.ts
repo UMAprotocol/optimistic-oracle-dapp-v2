1e3a94c263253cab90a7315ba2368686ca0e5de7
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-25 15:55:12 +0200

    add filters story
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { Button } from "./Button";
 export { ConnectButton } from "./ConnectButton";
+export { Filters } from "./Filters/Filters";
 export { GlobalStyle } from "./GlobalStyle";
 export { Header } from "./Header/Header";
 export { Layout } from "./Layout";
