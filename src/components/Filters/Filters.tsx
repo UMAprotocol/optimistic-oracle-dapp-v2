adcf7af9ba26aeba3672decd3943f63ce7a7e895
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-25 16:29:32 +0200

    add search bar
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Filters/Filters.tsx
+++ src/components/Filters/Filters.tsx
@@ -1,3 +1,17 @@
+import styled from "styled-components";
+import { Search } from "./Search";
+
 export function Filters() {
-  return <></>;
+  return (
+    <Wrapper>
+      <Search />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  min-height: 96px;
+  display: flex;
+  align-items: center;
+  background: var(--white);
+`;
--- src/components/Filters/Search.tsx
+++ src/components/Filters/Search.tsx
@@ -1,3 +1,44 @@
+import SearchIcon from "public/assets/search.svg";
+import { FormEvent, useState } from "react";
+import styled from "styled-components";
+
 export function Search() {
-  return <></>;
+  const [_input, setInput] = useState("");
+
+  function onInput(e: FormEvent<HTMLInputElement>) {
+    setInput(e.currentTarget.value);
+  }
+
+  return (
+    <Wrapper>
+      <SearchIconWrapper>
+        <SearchIcon aria-label="search input icon" />
+      </SearchIconWrapper>
+      <Input placeholder="Search" onInput={onInput} />
+    </Wrapper>
+  );
 }
+
+const Wrapper = styled.div`
+  position: relative;
+`;
+
+const SearchIconWrapper = styled.div`
+  position: absolute;
+  top: calc(50% - 12px);
+  left: 16px;
+`;
+
+const Input = styled.input`
+  min-width: 426px;
+  min-height: 48px;
+  padding-left: 50px;
+  color: var(--blue-grey-500);
+  font: var(--body-sm);
+  border: 1px solid var(--blue-grey-400);
+  border-radius: 24px;
+
+  &::placeholder {
+    color: var(--blue-grey-300);
+  }
+`;
