29ff839b541d3a1c9095c3018d204afef7ae7f8c
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-25 12:15:09 +0200

    add header component
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,18 @@
+import { Page } from "@/types";
 import styled from "styled-components";
 import { NavBar } from "./NavBar";
+import { Steps } from "./Steps";
 import { Title } from "./Title";
 import { VoteTicker } from "./VoteTicker";
 
-export function Header() {
+export function Header({ page }: { page: Page }) {
   return (
     <OuterWrapper>
       <InnerWrapper>
         <VoteTicker />
         <NavBar />
-        <Title />
+        <Title page={page} />
+        {page !== "settled" && <Steps page={page} />}
       </InnerWrapper>
     </OuterWrapper>
   );
@@ -18,9 +21,13 @@ export function Header() {
 const OuterWrapper = styled.header`
   min-height: var(--header-height);
   background: var(--blue-grey-700);
+  padding-top: 16px;
+  padding-bottom: 40px;
 `;
 
 const InnerWrapper = styled.div`
+  display: grid;
+  gap: 36px;
   max-width: var(--page-width);
   margin-inline: auto;
 `;
