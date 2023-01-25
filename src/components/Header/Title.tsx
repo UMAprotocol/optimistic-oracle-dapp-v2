d1cbb4d2926debbc07794ecb9161e462f55f0031
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-25 12:16:18 +0200

    add page title
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Header/Title.tsx
+++ src/components/Header/Title.tsx
@@ -1,13 +1,37 @@
+import { Page } from "@/types";
 import Box from "public/assets/box.svg";
 import styled from "styled-components";
 
-export function Title() {
+export function Title({ page }: { page: Page }) {
+  // todo: get these values from the sdk when implemented
+  const numVerifyStatements = 19;
+  const numProposeRequests = 256;
+  const numSettledStatements = 123456;
+
+  const pageTitles = {
+    verify: (
+      <>
+        Verify <strong>{numVerifyStatements}</strong> statements
+      </>
+    ),
+    propose: (
+      <>
+        Propose answers to <strong>{numProposeRequests}</strong> requests
+      </>
+    ),
+    settled: (
+      <>
+        View <strong>{numSettledStatements}</strong> settled statements
+      </>
+    ),
+  };
+
+  const pageTitle = pageTitles[page];
+
   return (
     <Wrapper>
       <BoxIcon />
-      <TitleText>
-        Verify <strong>19</strong> statements
-      </TitleText>
+      <TitleText>{pageTitle}</TitleText>
     </Wrapper>
   );
 }
