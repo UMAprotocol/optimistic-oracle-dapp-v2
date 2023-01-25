4243e0a19c07a7a41479dc6f4fd65545ae138627
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-25 12:14:46 +0200

    add page specific logic to layout
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -5,3 +5,13 @@ export function addOpacityToHsl(hsl: string, opacity: number) {
 }
 
 export const isExternalLink = (href: string) => !href.startsWith("/");
+
+export function capitalizeFirstLetter(str: string) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+export function determinePage(pathname: string) {
+  if (pathname === "/propose") return "propose";
+  if (pathname === "/settled") return "settled";
+  return "verify";
+}
