8a854a0fdfc1b6dd543537e00028bbeda5d90d10
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-25 12:17:02 +0200

    add placeholder pages for testing
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/pages/propose.tsx
@@ -0,0 +1,5 @@
+import { Layout } from "@/components";
+
+export default function Propose() {
+  return <Layout>rest of page</Layout>;
+}
---
+++ src/pages/settled.tsx
@@ -0,0 +1,5 @@
+import { Layout } from "@/components";
+
+export default function Settled() {
+  return <Layout>rest of page</Layout>;
+}
