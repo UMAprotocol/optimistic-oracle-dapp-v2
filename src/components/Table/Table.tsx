11cad0fb0179453ef6de18fa5925fe033b801a32
ryanwolhuter <dev@ryanwolhuter.com>
2023-01-27 10:40:34 +0200

    add table headers
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,5 +1,59 @@
 import { Page } from "@/types";
+import styled from "styled-components";
 
 export function Table({ page }: { page: Page }) {
-  return <>{page} table</>;
+  return (
+    <_Table>
+      <Headers page={page} />
+      <TBody>
+        <TR>
+          <TD>{page}</TD>
+        </TR>
+      </TBody>
+    </_Table>
+  );
 }
+
+function Headers({ page }: { page: Page }) {
+  const verify = [
+    "Query/Statement",
+    "Proposal/Assertion",
+    "Challenge period left",
+  ];
+
+  const propose = ["Query/Statement", "Type", "Bond", "Reward"];
+
+  const settled = ["Query/Statement", "Type", "Settled as"];
+
+  const headersForPages = {
+    verify,
+    propose,
+    settled,
+  };
+
+  const headers = headersForPages[page];
+
+  return (
+    <THead>
+      <TR>
+        {headers.map((header) => (
+          <TH key={header} scope="col">
+            {header}
+          </TH>
+        ))}
+      </TR>
+    </THead>
+  );
+}
+
+const _Table = styled.table``;
+
+const THead = styled.thead``;
+
+const TBody = styled.tbody``;
+
+const TR = styled.tr``;
+
+const TH = styled.th``;
+
+const TD = styled.td``;
