c4ad75238e1d9f2338b0fd2b23d21dad3d1823ac
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-01 10:03:25 +0200

    add placeholder types
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

---
+++ src/types/oracle.ts
@@ -0,0 +1,13 @@
+export type OracleQuery =
+  | Request
+  | Dispute
+  | PendingStatement
+  | AcceptedStatement;
+
+export type Request = {};
+
+export type Dispute = {};
+
+export type PendingStatement = {};
+
+export type AcceptedStatement = {};
