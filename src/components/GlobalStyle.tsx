868b76e2e589422e03f80d441848f56974a7db0d
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-01 11:33:12 +0200

    add text color aliases
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/GlobalStyle.tsx
+++ src/components/GlobalStyle.tsx
@@ -9,6 +9,7 @@ import {
   bodySm,
   bodyXl,
   bodyXs,
+  darkText,
   desktopHeaderHeight,
   desktopNavBarHeight,
   desktopPageWidth,
@@ -21,6 +22,7 @@ import {
   headerMd,
   headerSm,
   headerXs,
+  lightText,
   red100,
   red500,
   red500Opacity15,
@@ -215,6 +217,8 @@ a:not([class]) {
     --grey-400: ${grey400};
     --grey-500: ${grey500};
     --grey-900: ${grey900};
+    --dark-text: ${darkText}; /* alias for --blue-grey-700 */
+    --light-text: ${lightText}; /* alias for --white */
     /* Fonts */
     --header-lg: ${headerLg};
     --header-md: ${headerMd};
