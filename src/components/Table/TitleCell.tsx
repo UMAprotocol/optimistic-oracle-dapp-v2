9b114a4f62520375bc3d6e26f756ee1c9009e3a1
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-09 16:49:51 +0200

    add doc
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/components/Table/TitleCell.tsx
+++ src/components/Table/TitleCell.tsx
@@ -11,6 +11,16 @@ import {
   TitleWrapper,
 } from "./style";
 
+/**
+ * Renders the title cell for a table row
+ * This is the first cell in the row
+ * It shows the title, project, time, and chain name
+ * @param title - the title of the query
+ * @param project - the project of the query
+ * @param chainName - the chain name of the query
+ * @param timeFormatted - the time of the query
+ * @param expiryType - the expiry type of the query
+ */
 export function TitleCell({
   title,
   project,
