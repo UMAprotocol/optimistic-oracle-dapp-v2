5a1b9831361fe5400c9902fe13026dc4bc608005
david <david@umaproject.org>
2023-02-16 12:24:50 -0500

    feat: add various token queries
    Signed-off-by: david <david@umaproject.org>

--- src/contexts/OracleDataContext.tsx
+++ src/contexts/OracleDataContext.tsx
@@ -15,21 +15,38 @@ import {
   Requests,
   Assertion,
   Assertions,
+  Balances,
+  Allowances,
+  Tokens,
+  Balance,
+  Token,
+  Allowance,
 } from "@libs/oracle2";
-import { gql } from "@libs/oracle2/services";
+import { gql, tokens } from "@libs/oracle2/services";
 
 const gqlService = gql.Factory(config.subgraphs);
+// TODO: need to add configuration for providers and chains here
+const [tokenQueries, tokenService] = tokens.Factory([]);
+
+// tokenQueries has {token,allowance,balance} which you have to pass in chainid plus args for each call
+export { tokenQueries };
 
 export type OracleQueryList = OracleQueryUI[];
 export type OracleQueryTable = Record<string, OracleQueryUI>;
 export type RequestTable = Record<string, Request>;
 export type AssertionTable = Record<string, Assertion>;
 export type Errors = Error[];
+export type BalancesTable = Record<string, Balance>;
+export type AllowancesTable = Record<string, Allowance>;
+export type TokensTable = Record<string, Token>;
 export interface OracleDataContextState {
   all: OracleQueryTable | undefined;
   verify: OracleQueryList | undefined;
   propose: OracleQueryList | undefined;
   settled: OracleQueryList | undefined;
+  tokens: Tokens;
+  allowances: Allowances;
+  balances: Balances;
   errors: Errors;
 }
 
@@ -38,6 +55,9 @@ export const defaultOracleDataContextState: OracleDataContextState = {
   verify: undefined,
   propose: undefined,
   settled: undefined,
+  tokens: [],
+  allowances: [],
+  balances: [],
   errors: [],
 };
 
@@ -51,7 +71,15 @@ type DispatchAction<Type extends string, Data> = {
 };
 type ProcessRequestsAction = DispatchAction<"requests", Requests>;
 type ProcessAssertionsAction = DispatchAction<"assertions", Assertions>;
-type DispatchActions = ProcessRequestsAction | ProcessAssertionsAction;
+type ProcessBalancesAction = DispatchAction<"balances", Balances>;
+type ProcessTokensAction = DispatchAction<"tokens", Tokens>;
+type ProcessAllowancesAction = DispatchAction<"allowances", Allowances>;
+type DispatchActions =
+  | ProcessRequestsAction
+  | ProcessAssertionsAction
+  | ProcessBalancesAction
+  | ProcessTokensAction
+  | ProcessAllowancesAction;
 
 function DataReducerFactory<Input extends Request | Assertion>(
   converter: (input: Input) => OracleQueryUI
@@ -98,6 +126,41 @@ function DataReducerFactory<Input extends Request | Assertion>(
 const requestReducer = DataReducerFactory(requestToOracleQuery);
 const assertionReducer = DataReducerFactory(assertionToOracleQuery);
 
+function uniqueList<T>(list: T[], id: (el: T) => string): T[] {
+  const init: Record<string, T> = {};
+  return Object.values(
+    list.reduce((table, el) => {
+      table[id(el)] = el;
+      return table;
+    }, init)
+  );
+}
+
+function balancesReducer(state: OracleDataContextState, updates: Balances) {
+  return {
+    ...state,
+    balances: uniqueList([...state.balances, ...updates], (el: Balance) =>
+      [el.chainId, el.tokenAddress, el.account].join("~")
+    ),
+  };
+}
+function tokensReducer(state: OracleDataContextState, updates: Tokens) {
+  return {
+    ...state,
+    tokens: uniqueList([...state.tokens, ...updates], (el: Token) =>
+      [el.chainId, el.address].join("~")
+    ),
+  };
+}
+function allowancesReducer(state: OracleDataContextState, updates: Allowances) {
+  return {
+    ...state,
+    allowances: uniqueList([...state.allowances, ...updates], (el: Allowance) =>
+      [el.chainId, el.tokenAddress, el.account, el.spender].join("~")
+    ),
+  };
+}
+
 function oracleDataReducer(
   state: OracleDataContextState,
   action: DispatchActions
@@ -106,6 +169,12 @@ function oracleDataReducer(
     return requestReducer(state, action.data);
   } else if (action.type === "assertions") {
     return assertionReducer(state, action.data);
+  } else if (action.type === "balances") {
+    return balancesReducer(state, action.data);
+  } else if (action.type === "allowances") {
+    return allowancesReducer(state, action.data);
+  } else if (action.type === "tokens") {
+    return tokensReducer(state, action.data);
   }
   return state;
 }
@@ -120,10 +189,14 @@ export function OracleDataProvider({ children }: { children: ReactNode }) {
 
   useEffect(() => {
     // its important this client only gets initialized once
-    Client([gqlService], {
+    Client([gqlService, tokenService], {
       requests: (requests) => dispatch({ type: "requests", data: requests }),
       assertions: (assertions) =>
         dispatch({ type: "assertions", data: assertions }),
+      balances: (balances) => dispatch({ type: "balances", data: balances }),
+      allowances: (allowances) =>
+        dispatch({ type: "allowances", data: allowances }),
+      tokens: (tokens) => dispatch({ type: "tokens", data: tokens }),
       errors: setErrors,
     });
   }, []);
