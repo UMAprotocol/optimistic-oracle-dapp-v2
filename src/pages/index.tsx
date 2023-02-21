54c276a2e92ebbec6a28d3bcee68c2d98e52111a
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-21 11:16:32 +0200

    add filters to pages
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
-import { Layout, OracleQueries } from "@/components";
+import { Filters, Layout, OracleQueries } from "@/components";
 import { useOracleDataContext } from "@/hooks";
+import { mockFilters } from "@/stories/mocks";
 
 export default function Verify() {
   const { verify } = useOracleDataContext();
 
   return (
     <Layout>
+      <Filters {...mockFilters} />
       <OracleQueries
         queries={verify ?? []}
         isLoading={verify === undefined}
--- src/pages/propose.tsx
+++ src/pages/propose.tsx
@@ -1,11 +1,13 @@
-import { Layout, OracleQueries } from "@/components";
+import { Filters, Layout, OracleQueries } from "@/components";
 import { useOracleDataContext } from "@/hooks";
+import { mockFilters } from "@/stories/mocks";
 
 export default function Propose() {
   const { propose } = useOracleDataContext();
 
   return (
     <Layout>
+      <Filters {...mockFilters} />
       <OracleQueries
         queries={propose ?? []}
         isLoading={propose === undefined}
