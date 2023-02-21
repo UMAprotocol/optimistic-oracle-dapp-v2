7e5ad252a4133ee3d9a34b077f060f2895af663f
ryanwolhuter <dev@ryanwolhuter.com>
2023-02-21 11:16:42 +0200

    add mock filters
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/stories/Filters.stories.tsx
+++ src/stories/Filters.stories.tsx
@@ -1,5 +1,6 @@
 import { Filters } from "@/components";
 import type { Meta, StoryObj } from "@storybook/react";
+import { mockFilters } from "./mocks";
 
 const meta: Meta<typeof Filters> = {
   component: Filters,
@@ -10,52 +11,5 @@ export default meta;
 type Story = StoryObj<typeof Filters>;
 
 export const Default: Story = {
-  args: {
-    expiry: {
-      "Event-Based": {
-        checked: false,
-        count: 128,
-      },
-      "Time-Based": {
-        checked: false,
-        count: 128,
-      },
-    },
-    projects: {
-      Polymarket: {
-        checked: false,
-        count: 128,
-      },
-      UMA: {
-        checked: false,
-        count: 12,
-      },
-      "Cozy Finance": {
-        checked: false,
-        count: 50,
-      },
-      "stake.com": {
-        checked: false,
-        count: 0,
-      },
-    },
-    chains: {
-      Ethereum: {
-        checked: false,
-        count: 128,
-      },
-      Polygon: {
-        checked: false,
-        count: 12,
-      },
-      Optimism: {
-        checked: false,
-        count: 50,
-      },
-      Boba: {
-        checked: false,
-        count: 0,
-      },
-    },
-  },
+  args: mockFilters,
 };
--- src/stories/mocks.tsx
+++ src/stories/mocks.tsx
@@ -177,3 +177,52 @@ export const settledMockOracleQueryUIs = makeMockOracleQueryUIs({
     },
   ],
 });
+
+export const mockFilters = {
+  expiry: {
+    "Event-Based": {
+      checked: false,
+      count: 128,
+    },
+    "Time-Based": {
+      checked: false,
+      count: 128,
+    },
+  },
+  projects: {
+    Polymarket: {
+      checked: false,
+      count: 128,
+    },
+    UMA: {
+      checked: false,
+      count: 12,
+    },
+    "Cozy Finance": {
+      checked: false,
+      count: 50,
+    },
+    "stake.com": {
+      checked: false,
+      count: 0,
+    },
+  },
+  chains: {
+    Ethereum: {
+      checked: false,
+      count: 128,
+    },
+    Polygon: {
+      checked: false,
+      count: 12,
+    },
+    Optimism: {
+      checked: false,
+      count: 50,
+    },
+    Boba: {
+      checked: false,
+      count: 0,
+    },
+  },
+};
