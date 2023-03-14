697ce0c1bd7407b710aa54bb89310d073a731371
ryanwolhuter <dev@ryanwolhuter.com>
2023-03-14 09:22:23 +0200

    rename for clarity
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- libs/src/oracle2/services/index.ts
+++ libs/src/oracle2/services/index.ts
@@ -1,4 +1,4 @@
-export * as oracleV3Gql from "./assertions/gql";
+export * as assertionsGql from "./assertions/gql";
 export * as oracles from "./oracles";
-export * as oracleV1Gql from "./priceRequests/gql";
+export * as priceRequestsGql from "./priceRequests/gql";
 export * as tokens from "./tokens";
