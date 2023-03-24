b4ae896cbfcbb13eb7fa93bf6966197acefc024b
ryanwolhuter <dev@ryanwolhuter.com>
2023-03-24 09:41:09 +0200

    update regression tests
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/stories/Pagination.stories.tsx
+++ src/stories/Pagination.stories.tsx
@@ -53,7 +53,7 @@ const Template: Story = {
 export const Default: Story = {
   ...Template,
   args: {
-    entries: makeMockEntries(50),
+    entries: makeMockEntries(100),
   },
 };
 
@@ -93,16 +93,74 @@ export const OddNumberOfEntriesRegressionTest: Story = {
   play: async ({ canvasElement }) => {
     // use the parent element because the portal is outside the canvas (appended to body)
     const canvas = within(canvasElement.parentElement as HTMLElement);
-    const resultsPerPageDropdownButton = canvas.getByText("10 results");
+    const pageNumberButtons = () =>
+      Array.from(canvas.getByTestId("page-buttons").children)
+        .filter((child) => {
+          const _child = child as HTMLElement;
+          return !!_child.innerText && !isNaN(Number(_child.innerText));
+        })
+        .map((child) => child as HTMLElement);
+    const resultsPerPageDropdown = canvas.getByTestId("results-per-page");
+    const resultsPerPageDropdownButton = within(
+      resultsPerPageDropdown
+    ).getByRole("button");
     await waitFor(() => userEvent.click(resultsPerPageDropdownButton));
     const resultsPerPage50 = canvas.getByText("50 results");
     await waitFor(() => userEvent.click(resultsPerPage50));
-    const lastButton = canvas.getByText("3");
+    let lastButton = pageNumberButtons()[pageNumberButtons().length - 1];
     await waitFor(() => userEvent.click(lastButton));
+    // when we go to the last page with 50 results per page and there are 101 entries,
+    // the last entry (number 101) should be on the last page
+    // in this case, the first item and the last item are the same
+    expect(canvas.getByText("Entry 101")).toBeInTheDocument();
     await waitFor(() => userEvent.click(resultsPerPageDropdownButton));
     const resultsPerPage10 = canvas.getByText("10 results");
     await waitFor(() => userEvent.click(resultsPerPage10));
-    const lastButton11 = canvas.getByText("11");
-    expect(lastButton11).toHaveStyle({ color: white });
+    // when we go back to 10 results per page, the last entry should be on the 11th page
+    expect(canvas.getByText("Entry 101")).toBeInTheDocument();
+    // and the last page number button should be selected
+    lastButton = pageNumberButtons()[pageNumberButtons().length - 1];
+    expect(lastButton).toHaveStyle({ color: white });
+  },
+};
+
+export const EvenNumberOfEntriesRegressionTest: Story = {
+  ...Template,
+  args: {
+    entries: makeMockEntries(100),
+  },
+  play: async ({ canvasElement }) => {
+    // use the parent element because the portal is outside the canvas (appended to body)
+    const canvas = within(canvasElement.parentElement as HTMLElement);
+    const pageNumberButtons = () =>
+      Array.from(canvas.getByTestId("page-buttons").children)
+        .filter((child) => {
+          const _child = child as HTMLElement;
+          return !!_child.innerText && !isNaN(Number(_child.innerText));
+        })
+        .map((child) => child as HTMLElement);
+    const resultsPerPageDropdown = canvas.getByTestId("results-per-page");
+    const resultsPerPageDropdownButton = within(
+      resultsPerPageDropdown
+    ).getByRole("button");
+    await waitFor(() => userEvent.click(resultsPerPageDropdownButton));
+    const resultsPerPage50 = canvas.getByText("50 results");
+    await waitFor(() => userEvent.click(resultsPerPage50));
+    const lastButton = pageNumberButtons()[pageNumberButtons().length - 1];
+    await waitFor(() => userEvent.click(lastButton));
+    // when we go to the last page with 50 results per page and there are 100 entries,
+    // the last entry (number 100) should be on the last page
+    // in this case, the first item and the last item are different
+    let items = canvas.getAllByTestId("entry");
+    // the first item should be item 51
+    expect(items[0]).toHaveTextContent("Entry 51");
+    // the last item should be item 100
+    expect(items[items.length - 1]).toHaveTextContent("Entry 100");
+    await waitFor(() => userEvent.click(resultsPerPageDropdownButton));
+    const resultsPerPage10 = canvas.getByText("10 results");
+    await waitFor(() => userEvent.click(resultsPerPage10));
+    // the first item from when there were 50 results per page should still be the first item when there are 10 results per page
+    items = canvas.getAllByTestId("entry");
+    expect(items[0]).toHaveTextContent("Entry 51");
   },
 };
