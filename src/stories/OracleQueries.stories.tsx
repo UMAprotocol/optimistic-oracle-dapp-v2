02ad2c8f5e038c66eb67b6c40ee6eb3547b52433
ryanwolhuter <dev@ryanwolhuter.com>
2023-03-28 16:42:15 +0200

    update stories
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/stories/OracleQueries.stories.tsx
+++ src/stories/OracleQueries.stories.tsx
@@ -1,13 +1,6 @@
 import { OracleQueries, Panel } from "@/components";
 import { PanelProvider } from "@/contexts";
 import type { Meta, StoryObj } from "@storybook/react";
-import {
-  makeMockOracleQueryUIs,
-  makeRandomTitle,
-  proposeMockOracleQueryUIs,
-  settledMockOracleQueryUIs,
-  verifyMockOracleQueryUIs,
-} from "./mocks";
 
 const meta: Meta<typeof OracleQueries> = {
   component: OracleQueries,
@@ -28,56 +21,41 @@ type Story = StoryObj<typeof OracleQueries>;
 export const Propose: Story = {
   args: {
     page: "propose",
-    queries: proposeMockOracleQueryUIs(),
   },
 };
 
 export const Verify: Story = {
   args: {
     page: "verify",
-    queries: verifyMockOracleQueryUIs(),
   },
 };
 
 export const Settled: Story = {
   args: {
     page: "settled",
-    queries: settledMockOracleQueryUIs(),
   },
 };
 
 export const WithPagination: Story = {
   args: {
     page: "propose",
-    queries: makeMockOracleQueryUIs({
-      count: 100,
-      inputs: Array.from({ length: 100 }, () => ({
-        title: makeRandomTitle(),
-      })),
-    }),
   },
 };
 
 export const ProposeLoading: Story = {
   args: {
     page: "propose",
-    queries: [],
-    isLoading: true,
   },
 };
 
 export const VerifyLoading: Story = {
   args: {
     page: "verify",
-    queries: [],
-    isLoading: true,
   },
 };
 
 export const SettledLoading: Story = {
   args: {
     page: "settled",
-    queries: [],
-    isLoading: true,
   },
 };
