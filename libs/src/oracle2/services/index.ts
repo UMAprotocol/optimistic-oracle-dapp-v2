687d62e003dfc2f9f399e7774e98ffe810b3ecbd
david <david@umaproject.org>
2023-03-30 13:09:54 -0400

    improve: refac organization to include web3 logic
    Signed-off-by: david <david@umaproject.org>

--- libs/src/oracle2/services/index.ts
+++ libs/src/oracle2/services/index.ts
@@ -1,3 +1,7 @@
-export * as assertionsGql from "./assertions/gql";
+export * as oracle1Gql from "./oracle1/gql";
+// oracle 2 is handled the same way as oracle 1
+export * as oracle2Gql from "./oracle1/gql";
+// oracle skinny is same as oracle 1
+export * as oracle2SkinnyGql from "./oracle1/gql";
+export * as oracle3Gql from "./oracle3/gql";
 export * as oracles from "./oracles";
-export * as priceRequestsGql from "./priceRequests/gql";
