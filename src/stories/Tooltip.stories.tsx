e61b3c90f8b4f6eb09ceb3a91527ad7475e7b505
ryanwolhuter <dev@ryanwolhuter.com>
2023-04-04 11:32:28 +0200

    add tooltip animation
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/stories/Tooltip.stories.tsx
+++ src/stories/Tooltip.stories.tsx
@@ -1,8 +1,23 @@
 import { Tooltip } from "@/components";
 import type { Meta, StoryObj } from "@storybook/react";
+import { userEvent, waitFor, within } from "@storybook/testing-library";
 
 const meta: Meta<typeof Tooltip> = {
   component: Tooltip,
+  decorators: [
+    (Story) => (
+      <div
+        style={{
+          width: "100vh",
+          height: "100vh",
+          display: "grid",
+          placeItems: "center",
+        }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 };
 
 export default meta;
@@ -11,7 +26,9 @@ type Story = StoryObj<typeof Tooltip>;
 
 const Template: Story = {
   render: ({ children, content }) => (
-    <Tooltip content={content}>{children}</Tooltip>
+    <div data-testid="tooltip">
+      <Tooltip content={content}>{children}</Tooltip>
+    </div>
   ),
 };
 
@@ -20,5 +37,11 @@ export const Default: Story = {
   args: {
     children: "show tooltip",
     content: "tooltip content",
+  },
+  play: async ({ canvasElement }) => {
+    // use the parent element because the portal is outside the canvas (appended to body)
+    const canvas = within(canvasElement.parentElement as HTMLElement);
+    const tooltip = (await canvas.findByTestId("tooltip")).children[0];
+    await waitFor(() => userEvent.hover(tooltip));
   },
 };
