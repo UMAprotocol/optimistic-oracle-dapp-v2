9c150234949c9f9c3cc9115205ef55992a66d3cc
ryanwolhuter <dev@ryanwolhuter.com>
2023-05-18 15:52:10 +0200

    move error message type to shared
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/stories/pages/types.ts
+++ src/stories/pages/types.ts
@@ -2,7 +2,7 @@ import type { NotificationsById } from "@/contexts";
 import type VerifyPage from "@/pages";
 import type ProposePage from "@/pages/propose";
 import type SettledPage from "@/pages/settled";
-import type { ErrorMessage } from "@/types";
+import type { ErrorMessage } from "@shared/types";
 import type { StoryObj } from "@storybook/react";
 
 export type Page = typeof VerifyPage | typeof ProposePage | typeof SettledPage;
