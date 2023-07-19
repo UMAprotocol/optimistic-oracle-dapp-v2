bd7e823c463225239447e6be00bbcf09ea4b4292
ryanwolhuter <dev@ryanwolhuter.com>
2023-07-19 15:17:35 +0200

    if this works I am going to be upset
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- next.config.js
+++ next.config.js
@@ -26,6 +26,15 @@ const nextConfig = {
       },
     ];
   },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': [
+        'node_modules/@swc/core-linux-x64-gnu',
+        'node_modules/@swc/core-linux-x64-musl',
+        'node_modules/@esbuild/linux-x64',
+      ],
+    },
+  },
 };
 
 module.exports = withSvgr(nextConfig);
