c4008dc9892ba0ebd1389fc0a814074c3e9cc262
ryanwolhuter <dev@ryanwolhuter.com>
2023-07-21 10:40:37 +0200

    use strings for metadata
    Signed-off-by: ryanwolhuter <dev@ryanwolhuter.com>

--- src/app/propose/page.tsx
+++ src/app/propose/page.tsx
@@ -1,4 +1,9 @@
 import Propose from "@/components/pages/propose";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Optimistic Oracle V2 | Propose",
+};
 
 export default function ProposePage() {
   return <Propose />;
--- src/app/settled/page.tsx
+++ src/app/settled/page.tsx
@@ -1,4 +1,9 @@
 import Settled from "@/components/pages/settled";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Optimistic Oracle V2 | Settled",
+};
 
 export default function SettledPage() {
   return <Settled />;
