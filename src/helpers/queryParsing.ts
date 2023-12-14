6e4172be164e9b721061736acc41fad98b307893
Gerhard Steenkamp <gerhard@umaproject.org>
2023-12-14 13:27:19 +0200

    all commas in p0 & p1 res

--- src/helpers/queryParsing.ts
+++ src/helpers/queryParsing.ts
@@ -190,7 +190,7 @@ function dynamicPolymarketOptions(
     /res_data: (p\d): (\d+\.\d+|\d+), (p\d): (\d+\.\d+|\d+), (p\d): (\d+\.\d+|\d+)/,
   );
   const correspondence = decodedAncillaryData.match(
-    /Where (p\d) corresponds to ([^,]+), (p\d) to ([^,]+), (p\d) to ([^.,]+)/,
+    /Where (p\d) corresponds to ((?:[^,]|,(?!\s))+), (p\d) to ((?:[^,]|,(?!\s))+), (p\d) to ([^.,]+)/,
   );
 
   if (!resData || !correspondence) return [];
