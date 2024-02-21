63eb7d139e396d6d98545391ea80f78960726266
david <david@umaproject.org>
2024-02-21 14:06:22 -0500

    fix: allow more than 5k gql queries for oov1/2
    Signed-off-by: david <david@umaproject.org>

--- libs/src/oracle-sdk-v2/services/oraclev1/gql/queries.ts
+++ libs/src/oracle-sdk-v2/services/oraclev1/gql/queries.ts
@@ -29,7 +29,9 @@ async function fetchAllRequests(url: string, queryName: string, isV2: boolean) {
     url,
     makeQuery(queryName, isV2, first, skip)
   );
-  while (requests.length > 0) {
+
+  // thegraph wont allow skip > 5000,
+  while (requests.length > 0 && skip < 5000) {
     result.push(...requests);
     skip += first;
     requests = await fetchPriceRequests(
@@ -37,7 +39,21 @@ async function fetchAllRequests(url: string, queryName: string, isV2: boolean) {
       makeQuery(queryName, isV2, first, skip)
     );
   }
+  
+  // have to use time based logic after we run out of skip size
+  while (requests.length === first) {
+    result.push(...requests);
+    const lastTime = requests[requests.length-1].time;
+    requests = await fetchPriceRequests(
+      url,
+      makeTimeBasedQuery(queryName,isV2,first,Number(lastTime))
+    )
+  }
+
+  result.push(...requests);
+
   return result;
+  
 }
 
 async function fetchPriceRequests(url: string, query: string) {
@@ -59,6 +75,63 @@ function makeQuery(
   const query = gql`
   query ${queryName} {
     optimisticPriceRequests(orderBy: time, orderDirection: desc, first: ${first}, skip: ${skip}) {
+      id
+      identifier
+      ancillaryData
+      time
+      requester
+      currency
+      reward
+      finalFee
+      proposer
+      proposedPrice
+      proposalExpirationTimestamp
+      disputer
+      settlementPrice
+      settlementPayout
+      settlementRecipient
+      state
+      requestTimestamp
+      requestBlockNumber
+      requestHash
+      requestLogIndex
+      proposalTimestamp
+      proposalBlockNumber
+      proposalHash
+      proposalLogIndex
+      disputeTimestamp
+      disputeBlockNumber
+      disputeHash
+      disputeLogIndex
+      settlementTimestamp
+      settlementBlockNumber
+      settlementHash
+      settlementLogIndex
+      ${
+        isV2
+          ? `
+            customLiveness
+            bond
+            eventBased
+            `
+          : ""
+      }
+    }
+  }
+`;
+
+  return query;
+}
+
+function makeTimeBasedQuery(
+  queryName: string,
+  isV2: boolean,
+  first: number,
+  lastTime: number,
+) {
+  const query = gql`
+  query ${queryName} {
+    optimisticPriceRequests(orderBy: time, orderDirection: desc, first: ${first}, where: { time_lt: ${lastTime}}) {
       id
       identifier
       ancillaryData
