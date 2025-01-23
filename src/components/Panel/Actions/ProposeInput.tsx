ba723cfd17bbf4c602dd20830c9216557de07911
Gerhard Steenkamp <gerhard@umaproject.org>
2025-01-23 12:24:00 +0200

    style propose inputs

--- src/components/Panel/Actions/ProposeInput.tsx
+++ src/components/Panel/Actions/ProposeInput.tsx
@@ -61,29 +61,44 @@ function MultipleValuesInput(props: MultipleInputProps) {
   return (
     <div
       className={cn(
-        "flex gap-2 items-start justify-between w-full mb-2 relative",
-        { "flex-col": items?.length > 2 },
+        "flex flex-col gap-2 items-start justify-between w-full mb-2 relative",
       )}
     >
-      {items.map(([label, value]) => (
-        <label
-          key={label}
-          htmlFor={`input-${label}`}
-          className="flex flex-col gap-2 font-bold "
-        >
-          {label}
-          <DecimalInput
-            disabled={props.isUnresolvable}
-            id={`input-${label}`}
-            placeholder={"Enter score"} // TODO: make this dynamic
-            maxDecimals={0}
-            value={value}
-            onInput={(_val) => props.onChange({ label, value: _val })}
-            addErrorMessage={props.addErrorMessage}
-            removeErrorMessage={props.removeErrorMessage}
-          />
-        </label>
-      ))}
+      <div
+        className={cn(
+          "flex gap-5 items-start justify-between w-full mb-2 relative",
+          { "flex-col gap-2": items?.length > 2 },
+        )}
+      >
+        {items.map(([label, value], i) => (
+          <>
+            <label
+              key={label}
+              htmlFor={`input-${label}`}
+              className="flex flex-1 w-full flex-col gap-2 font-normal"
+            >
+              {label}
+              <DecimalInput
+                disabled={props.isUnresolvable}
+                id={`input-${label}`}
+                placeholder={
+                  props.isUnresolvable ? "Unresolvable" : "Enter score"
+                }
+                maxDecimals={0}
+                value={props.isUnresolvable ? "" : value}
+                onInput={(_val) => props.onChange({ label, value: _val })}
+                addErrorMessage={props.addErrorMessage}
+                removeErrorMessage={props.removeErrorMessage}
+              />
+            </label>
+            {i === 0 && items?.length === 2 && (
+              <span className="font-normal text-4xl text-[#A2A1A5] relative top-8">
+                -
+              </span>
+            )}
+          </>
+        ))}
+      </div>
       <StyledCheckbox
         checked={props.isUnresolvable}
         itemName="Unresolvable"
