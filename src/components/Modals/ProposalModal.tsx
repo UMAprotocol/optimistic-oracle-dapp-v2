af8959e8afecc9c6fec398e43cf8dae60460dabb
Gerhard Steenkamp <gerhard@umaproject.org>
2025-03-19 12:39:58 +0200

    style checkbox
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/components/Modals/ProposalModal.tsx
+++ src/components/Modals/ProposalModal.tsx
@@ -1,6 +1,10 @@
+"use client";
+
+import { useLocalStorage } from "usehooks-ts";
 import { Button } from "../Button";
 import { Modal, type ModalProps } from "./Modal";
 import { TriangleAlert } from "lucide-react";
+import { Checkbox } from "../ui/checkbox";
 
 export type ProposalModalProps = Omit<ModalProps, "children"> & {
   onContinue: () => void;
@@ -12,11 +16,16 @@ export function ProposalModal({
   onContinue,
   ...props
 }: ProposalModalProps) {
+  const [showWarning, setShowWarning] = useLocalStorage(
+    "show-early-proposal-warning",
+    true,
+  );
+
   return (
     <Modal {...props}>
       <div className="flex flex-col w-full gap-4">
-        <div className="flex justify-start gap-2">
-          <TriangleAlert className="text-red-500" />
+        <div className="flex justify-start gap-2 pr-4">
+          <TriangleAlert className="text-red-500 shrink-0" />
           <span className="font-bold">
             Are you sure this proposal is not Too Early?
           </span>
@@ -26,12 +35,29 @@ export function ProposalModal({
           disputed and resolved TOO EARLY which results in the proposer losing
           their bond. 82% of successful disputes are resolved TOO EARLY. Read
           more{" "}
-          <a href="https://blog.uma.xyz/articles/what-is-p4" target="_blank">
-            here
-          </a>{" "}
-          .
+          <a
+            href="https://blog.uma.xyz/articles/what-is-p4"
+            target="_blank"
+            rel="noreferrer"
+            className="text-red-600 underline hover:no-underline "
+          >
+            here.
+          </a>
         </p>
-        <div className="flex-col flex sm:flex-row w-full justify-between gap-2">
+        <label
+          className="flex py-1 gap-2 items-center cursor-pointer"
+          htmlFor="show-early-proposal-checkbox"
+        >
+          <Checkbox
+            id="show-early-proposal-checkbox"
+            className="pl-0"
+            onCheckedChange={(checked) => setShowWarning(!checked)}
+            checked={!showWarning}
+          />
+          Don&apos;t show this message again
+        </label>
+
+        <div className="flex-col mt-2 flex sm:flex-row w-full justify-between gap-2">
           <Button width="100%" onClick={onContinue} variant="primary">
             Continue and sign
           </Button>
