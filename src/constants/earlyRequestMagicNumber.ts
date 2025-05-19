b1ffbab50bfc5facc6ff254cbeeb921a2c45262c
Gerhard Steenkamp <gerhard@umaproject.org>
2025-05-19 21:11:43 +0200

    refactor
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/constants/earlyRequestMagicNumber.ts
+++ src/constants/earlyRequestMagicNumber.ts
@@ -1,6 +1,5 @@
 import { isBigNumberish } from "@ethersproject/bignumber/lib/bignumber";
 import { isDefined } from "@libs/utils";
-import { bigNumberFromFloatString } from "@shared/utils";
 
 export const earlyRequestMagicNumber =
   "-57896044618658097711785492504343953926634992332820282019728.792003956564819968";
@@ -8,6 +7,13 @@ export const earlyRequestMagicNumberWei =
   "-57896044618658097711785492504343953926634992332820282019728792003956564819968";
 export const earlyRequestMagicNumberRounded =
   "-57896044618658097711785492504343953926634992332820282019728";
+
+const magicNumberIntegerParts = [
+  earlyRequestMagicNumber,
+  earlyRequestMagicNumberWei,
+  earlyRequestMagicNumberRounded,
+].map(takeInteger);
+
 /**
  * Description
  * @param {string} value this string is most likely formatted as localeString (includes commas, to a precision of 2)
@@ -16,25 +22,16 @@ export const earlyRequestMagicNumberRounded =
 export function isEarlyVote(value: string | undefined | null): boolean {
   if (!isDefined(value)) return false;
 
-  const asBigInt = takeIntegerAsBigNumber(value);
-  if (!asBigInt) {
+  // Extract just the integer part of the input value
+  const integerPart = takeInteger(value);
+  if (!isBigNumberish(integerPart)) {
     return false;
   }
-  return [
-    earlyRequestMagicNumber,
-    earlyRequestMagicNumberWei,
-    earlyRequestMagicNumberRounded,
-  ]
-    .map(takeIntegerAsBigNumber)
-    .filter(Boolean)
-    .some((num) => num.eq(asBigInt));
+
+  // Direct string comparison of integer parts
+  return magicNumberIntegerParts.includes(integerPart);
 }
 
-function takeIntegerAsBigNumber(value: string) {
-  const maybeBn = value.replace(/,/g, "").split(".")[0];
-  if (!isBigNumberish(maybeBn)) {
-    return;
-  }
-  // remove commas, if present, remove decimals, take only integer
-  return bigNumberFromFloatString(maybeBn);
+function takeInteger(value: string) {
+  return value.replace(/,/g, "").split(".")[0];
 }
