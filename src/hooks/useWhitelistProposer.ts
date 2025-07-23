ed2512e64c3ae345a0e4c84e2be7ef796222fb39
Gerhard Steenkamp <gerhard@umaproject.org>
2025-07-23 14:35:10 +0200

    stub whitelist check
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

---
+++ src/hooks/useWhitelistProposer.ts
@@ -0,0 +1,24 @@
+import { sleep } from "@libs/utils";
+import { useAccount, useQuery } from "wagmi";
+
+async function isUserWhitelisted(
+  address: string | undefined,
+): Promise<boolean> {
+  console.log(`Checking if ${address} is whitelisted`);
+  await sleep(500);
+  // 1. connect to ManagedOO contract
+  // 2. call view function getProposerWhitelistWithEnforcementStatus
+  // 3. check `isEnforced`
+  // 4. check `allowedProposers`
+  // 5. resolve user status, return
+
+  return false;
+}
+
+export function useIsUserWhitelisted() {
+  const { address } = useAccount();
+  return useQuery(["whitelist-proposer", address], {
+    queryFn: () => isUserWhitelisted(address),
+    refetchInterval: 30_000, // 30 seconds
+  });
+}
