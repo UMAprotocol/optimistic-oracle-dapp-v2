520852415408af615ea18317cd3b692c468d9208
Gerhard Steenkamp <gerhard@umaproject.org>
2025-07-23 21:28:49 +0200

    adds hooks to fetch whitelist
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/hooks/useWhitelistProposer.ts
+++ src/hooks/useWhitelistProposer.ts
@@ -1,24 +1,98 @@
-import { shortenIfAddress, sleep } from "@libs/utils";
+import { getContract } from "@/helpers";
+import type { OracleQueryUI } from "@/types";
+import type { OptimisticOracleV2Ethers } from "@uma/contracts-node";
+import assert from "assert";
+import type { BytesLike } from "ethers";
 import { useAccount, useQuery } from "wagmi";
 
-async function isUserWhitelisted(
-  address: string | undefined,
-): Promise<boolean> {
-  console.log(`Checking if ${shortenIfAddress(address)} is whitelisted`);
-  await sleep(500);
-  // 1. connect to ManagedOO contract
-  // 2. call view function getProposerWhitelistWithEnforcementStatus
-  // 3. check `isEnforced`
-  // 4. check `allowedProposers`
-  // 5. resolve user status, return
+export type ProposerWhitelistWithEnforcementStatus = {
+  allowedProposers: string[];
+  isEnforced: boolean;
+};
 
-  return false;
+// TODO: replace this stub when @uma/contracts-node is updated
+type ManagedOptimisticOracleV2 = OptimisticOracleV2Ethers & {
+  getProposerWhitelistWithEnforcementStatus: (
+    requester: string,
+    identifier: BytesLike,
+    ancillaryData: BytesLike,
+  ) => Promise<ProposerWhitelistWithEnforcementStatus>;
+};
+
+async function getProposerWhitelistWithEnforcementStatus(query: OracleQueryUI) {
+  try {
+    const { requester, identifier, queryTextHex: ancillaryData } = query;
+    assert(requester, "requester missing from query object");
+    assert(identifier, "identifier missing from query object");
+    assert(ancillaryData, "ancillaryData missing from query object");
+
+    const contract = (await getContract(
+      "OptimisticOracleV2", // TODO: replace "OptimisticOracleV2" with "ManagedOptimisticOracleV2"
+      query.chainId,
+    )) as ManagedOptimisticOracleV2;
+    return await contract.getProposerWhitelistWithEnforcementStatus(
+      requester,
+      identifier,
+      ancillaryData,
+    );
+  } catch (e) {
+    // TODO: handle errors, add retry logic?
+    console.error(e);
+    throw e;
+  }
 }
 
-export function useIsUserWhitelisted() {
-  const { address } = useAccount();
-  return useQuery(["whitelist-proposer", address], {
-    queryFn: () => isUserWhitelisted(address),
+type QueryOptions = Omit<Parameters<typeof useQuery>[1], "queryFn">;
+
+export function useProposerWhitelist(
+  query: OracleQueryUI | undefined,
+  queryOptions?: QueryOptions,
+) {
+  return useQuery(["getProposerWhitelistWithEnforcementStatus", query?.id], {
+    queryFn: () => {
+      if (!query) return undefined;
+      return getProposerWhitelistWithEnforcementStatus(query);
+    },
     refetchInterval: 30_000, // 30 seconds
+    enabled: Boolean(query),
+    onError(err) {
+      console.error({
+        error: err,
+        at: "useProposerWhitelist",
+      });
+    },
+    ...queryOptions,
+  });
+}
+
+export function useIsUserWhitelisted(
+  query: OracleQueryUI | undefined,
+  queryOptions?: QueryOptions,
+) {
+  const { data } = useProposerWhitelist(query, {
+    enabled: Boolean(
+      query && query.oracleType !== "Managed Optimistic Oracle V2",
+    ),
+  });
+  const { address } = useAccount();
+  return useQuery(["useIsUserWhitelisted", address, query?.id], {
+    queryFn: () => {
+      if (!query) return false;
+      if (query.oracleType !== "Managed Optimistic Oracle V2") return true;
+      if (!data || !address) {
+        return false;
+      }
+      if (!data.isEnforced) return true;
+      return data.allowedProposers.includes(address);
+    },
+    refetchInterval: 30_000, // 30 seconds
+    enabled: Boolean(query),
+    onError(err) {
+      console.error({
+        error: err,
+        at: "useIsUserWhitelisted",
+      });
+    },
+    ...queryOptions,
   });
 }
