db75e23b5ac4e94d7b5c654c8a1cc33254270388
Gerhard Steenkamp <gerhard@umaproject.org>
2025-08-25 10:45:12 +0200

    add managed oo banners and organize
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

---
+++ src/components/Link.tsx
@@ -0,0 +1,35 @@
+import { cn } from "@/helpers";
+import { ExternalLinkIcon } from "lucide-react";
+import type { LinkProps as NextLinkProps } from "next/link";
+import NextLink from "next/link";
+import Url from "url";
+
+export type LinkProps = NextLinkProps & {
+  children: React.ReactNode;
+  externalIcon?: boolean;
+  className?: string;
+};
+
+export function Link({
+  className,
+  children,
+  externalIcon = false,
+  ...props
+}: LinkProps) {
+  const isInternal = Url.format(props.href).toString().startsWith("/");
+  return (
+    <NextLink
+      className={cn(
+        "group group-hover:underline inline items-center gap-2 ",
+        className,
+      )}
+      target={!isInternal ? "_blank" : "_self"}
+      {...props}
+    >
+      {children}
+      {!isInternal && externalIcon && (
+        <ExternalLinkIcon className="ml-1 inline w-[1em] h-[1em]" />
+      )}
+    </NextLink>
+  );
+}
