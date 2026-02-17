c4da3f01e850457c4908b252f34968fd88d67ff6
Gerhard Steenkamp <gerhard@umaproject.org>
2026-02-17 15:00:02 +0200

    fix bug in isValidOracleType
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -258,7 +258,7 @@ export function isValidChainId(
 export function isValidOracleType(
   oracleType: string | undefined,
 ): oracleType is OracleType {
-  return !!oracleType && oracleType in oracleTypes;
+  return !!oracleType && oracleTypes.includes(oracleType as OracleType);
 }
 
 export function isWagmiAddress(
