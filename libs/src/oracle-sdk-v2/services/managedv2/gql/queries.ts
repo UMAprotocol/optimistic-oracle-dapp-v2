67df5d56b2039ddbb4d34bf184466606c20329df
Gerhard Steenkamp <gerhard@umaproject.org>
2026-03-23 12:33:03 +0200

    fixup
    Signed-off-by: Gerhard Steenkamp <gerhard@umaproject.org>

--- libs/src/oracle-sdk-v2/services/managedv2/gql/queries.ts
+++ libs/src/oracle-sdk-v2/services/managedv2/gql/queries.ts
@@ -156,6 +156,7 @@ export async function getProposeRequests(
         ) { ${managedFields} }
       }
     `,
+    Infinity,
   );
 }
 
--- libs/src/oracle-sdk-v2/services/oraclev1/gql/queries.ts
+++ libs/src/oracle-sdk-v2/services/oraclev1/gql/queries.ts
@@ -165,6 +165,7 @@ export async function getProposeRequests(
         ) { ${fields} }
       }
     `,
+    Infinity,
   );
 }
 
